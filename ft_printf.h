c839a2794a0c36c9463e2bfaa61adc765b3fcad7
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-05-17 04:52:45 +0200

    Update library

--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,15 @@
 /*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 17:28:18 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/05/12 20:13:40 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/05/13 22:41:54 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # define CONV_TYPE "cspdiuxX%"
+# define HEXADECIMAL_X "0123456789ABCDEF"
+# define HEXADECIMAL_x "0123456789abcdef"
 
 # include <unistd.h>
 # include <stdarg.h>
@@ -23,4 +25,16 @@
 int	ft_printf(const char *format);
 int count_input(char *f_string);
 void	fill_inputs(char ***inputs, char *f_string);
+char	*hex_concat(char *src, char c);
+void	ft_putnbr_base(long long int nbr, char *base, char **output);
+int	check_base(char *base);
+char	*p_conv(va_list arg);
+char	*c_conv(va_list arg);
+char	*s_conv(va_list arg);
+char	*ft_utoa(unsigned int n);
+char	*id_conv(va_list arg);
+char	*u_conv(va_list arg);
+char	*x_conv(va_list arg);
+char	*X_conv(va_list arg);
+
 #endif
